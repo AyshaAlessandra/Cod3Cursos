1362076a0dc066c96d345778a2d4ebae259e0446
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-09-26 19:32:22 -0300

    Primeiro programa

---
+++ CursoCSharp/CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class PrimeiroPrograma
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Este é o meu ");
+            Console.WriteLine("primeiro programa");
+            Console.WriteLine("nesta linguagem!");
+        }
+    }
+}
