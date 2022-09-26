853d3c29f67f97ff84ece76d6466f741f75ada08
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-09-26 20:26:25 -0300

    Comentários

---
+++ CursoCSharp/CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    /// <summary>
+    /// Aqui eu posso fazer o resumo do que o método faz
+    /// ou usar tags para dar significado a documentação.
+    /// </summary>
+    internal class Comentarios
+    {
+        public static void Executar()
+        {
+            //Cuidado com os comentários desnecessários
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            Console.WriteLine("O C# tem XML Comments");
+
+        }
+    }
+}
