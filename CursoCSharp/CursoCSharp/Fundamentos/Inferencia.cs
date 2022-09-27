0c69f9a36143b9c2f35560f256ff0a8351320f86
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-09-27 11:26:17 -0300

    Interferência de Tipos

---
+++ CursoCSharp/CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class Inferencia
+    {
+        public static void Executar()
+        {
+          /* É obrigatório inicializar uma várivavel do tipo implicita 'var', se eu apenas declarar
+           * da erro. Isso acontece porque o C# vai identificar por d'baixo dos panos, qual tipo
+           * é a variável e irá atribuir o tipo certo.
+           */
+
+            var nome = "Aysha";
+            //nome = 123
+            Console.WriteLine($"Nome: {nome}");
+
+            //int idade;
+            var idade = 21;
+            Console.WriteLine($"Idade: {idade}");
+
+            int a;
+            a = 7;
+
+            int b = 4;
+            Console.WriteLine($"Soma: {a + b}");
+        }
+    }
+}
