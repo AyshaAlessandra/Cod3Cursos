6dd2605620677f40a00d7feb0f3eff288e989f1a
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-09-27 14:52:45 -0300

    Conversões

---
+++ CursoCSharp/CursoCSharp/Fundamentos/Conversoes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class Conversoes
+    {
+        public static void Executar()
+        {
+            int inteiro = 10;
+            double quebrado = inteiro;
+            Console.WriteLine(quebrado);
+            
+            /*Aqui, vou ter que fazer explicitamente a conversão de um tipo para  o outro
+             * ciente que irei ter perda de dados. Esse procedimento é chamado de casting
+             */
+
+            double nota = 9.7;
+            int notaTruncada = (int) nota;
+            Console.WriteLine("Nota truncada: {0}", notaTruncada);
+
+            Console.Write("Digite sua idade: ");
+            string idadeString = Console.ReadLine();
+            int idadeInteiro = int.Parse(idadeString);
+            Console.WriteLine($"Idade inserida: {idadeInteiro}");
+
+            //Outra forma de converter string para inteiro.
+            idadeInteiro = Convert.ToInt32(idadeString);
+            Console.WriteLine("Resultado: {0}", idadeInteiro);
+
+            //uma forma mais segura de converter uma string, se eu digitar uma palavra e não um 
+            //múmero, vai me retornar um 0, e não da um erro.
+            
+            Console.Write("Digite o primeiro número: ");
+            string palavra = Console.ReadLine();
+            int numero1;
+            int.TryParse(palavra, out numero1);
+            Console.WriteLine("Resultado 1: {0}", numero1);
+
+            Console.Write("Digite o segundo número: ");
+            int.TryParse(Console.ReadLine(), out int numero2);
+            Console.WriteLine("Resultado 2: {0}", numero2);
+        }
+    }
+}
