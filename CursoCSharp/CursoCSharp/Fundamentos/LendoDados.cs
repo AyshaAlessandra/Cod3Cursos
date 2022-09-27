e204881e1cf871430f739fdf2a22cf819fc5033e
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-09-27 12:45:37 -0300

    Lendo Dados do Console

---
+++ CursoCSharp/CursoCSharp/Fundamentos/LendoDados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class LendoDados
+    {
+        public static void Executar()
+        {
+            Console.Write("Qual é o seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Me informe sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Qual é o seu salário? ");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           
+            /* CultureInfo.InvariantCulture, serve para trocar o ponto por vírgula na hora
+             * da digitação. Para usar ele, precisa por o System.Globalization.
+             */
+
+            Console.WriteLine($"{nome} tem {idade} anos e recebe {salario} reais.");
+        }
+    }
+}
