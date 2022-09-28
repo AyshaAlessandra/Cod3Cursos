bca682b85f07c4b5f0ebf6f1a8f0b5a1231a1286
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-09-28 09:19:50 -0300

    Operadores Aritméticos

---
+++ CursoCSharp/CursoCSharp/Fundamentos/OperadoresAritmeticos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class OperadoresAritmeticos
+    {
+        public static void Executar()
+        {
+            //Preço Desconto
+            var preco = 1000.0;
+            var imposto = 355;
+            var desconto = 0.1;
+
+            double total = preco + imposto;
+            var totalComDesconto = total - (total * desconto);
+            Console.WriteLine("O preço final é: {0}", totalComDesconto);
+
+            //IMC
+            double peso = 85;
+            double altura = 1.77;
+            double imc = peso / Math.Pow(altura,2);//numero elevado ao quadrado
+            Console.WriteLine($"IMC é: {imc.ToString("F2")}.");
+
+            //Número Par/Impar
+            int par = 24;
+            int impar = 55;
+            Console.WriteLine($"{par}/2 tem resto {par % 2}");
+            Console.WriteLine($"{impar}/2 tem resto {impar % 2}");
+        }
+    }
+}
