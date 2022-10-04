b7e7950dc3efa0250ad86becd0068910142616df
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-10-04 09:50:36 -0300

    Desafio Acessar Atributo

---
+++ CursoCSharp/CursoCSharp/ClassesEMetodos/DesafioAtributo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    internal class DesafioAtributo
+    {
+        //atributo de instancia
+        int a = 10;
+
+        //atributo de classe 
+        public static void Executar()
+        {
+            DesafioAtributo desafioAtributo = new DesafioAtributo();
+
+            Console.WriteLine(desafioAtributo.a);
+        }
+    }
+}
+/*Um método que pertence a classe não consegue acessar um atributo de instancia.
+ * Então precisisei criar uma instancia do da classe na qual estava.
+ */
