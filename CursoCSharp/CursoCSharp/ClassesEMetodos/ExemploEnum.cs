f1b80d0e31d974bb08e33f98fa10279dbc0858db
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-10-04 13:59:38 -0300

    Enumerações (Enum)

---
+++ CursoCSharp/CursoCSharp/ClassesEMetodos/ExemploEnum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public enum Genero {Acao, Aventura, Terro, Animacao, Comedia};
+
+    public class Filme
+    {
+        public string Titulo;
+        public Genero GeneroDoFilme;
+    }
+
+    internal class ExemploEnum
+    {
+        public static void Executar()
+        {
+            int id = (int)Genero.Animacao;
+            Console.WriteLine(id);
+
+            Filme filmeParaFamilia = new Filme();
+            filmeParaFamilia.Titulo = "Sing 2";
+            filmeParaFamilia.GeneroDoFilme = Genero.Animacao;
+            Console.WriteLine($"O filme {filmeParaFamilia.Titulo} é" +
+                $" {filmeParaFamilia.GeneroDoFilme}.");
+        }
+    }
+}
