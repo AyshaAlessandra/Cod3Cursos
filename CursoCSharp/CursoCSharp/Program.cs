88f74239fa85ce8d9aba0a573413d953cbb44725
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-10-06 12:03:48 -0300

    Construtor: Usando o this

--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -72,6 +72,7 @@ namespace CursoCSharp {
 
                 //OO
                 {"Herança - OO", Heranca.Executar},
+                {"Construtor This - OO", ConstrutorThis.Executar},
             });
 
             central.SelecionarEExecutar();
