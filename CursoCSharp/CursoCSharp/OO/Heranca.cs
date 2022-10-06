ffdbf5bf74bfc4250678aa03e84c3bf57abd684a
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-10-06 11:18:14 -0300

    Herança #02

--- CursoCSharp/CursoCSharp/OO/Heranca.cs
+++ CursoCSharp/CursoCSharp/OO/Heranca.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace CursoCSharp.OO
 {
+    /*Quando não há um construtor padrão na classe pai, quando outra classe herdar, irá
+     * precisar por um construtor base.
+     */
+
     public class Carro
     {
         protected readonly int VelocidadeMaxima;
@@ -31,8 +35,9 @@ namespace CursoCSharp.OO
             }
             return VelocidadeAtual;
         }
-
-        public int Acelerar()
+        //Quando coloco o modificador virtual, quero dizer que o método pode ser sobrescrito.
+        //Se não colocar, o método não poderá ser sobrescrito (override).
+        public virtual int Acelerar()
         {
             return AlterarVelocidade(5);
         }
@@ -57,6 +62,17 @@ namespace CursoCSharp.OO
         {
 
         }
+
+        public override int Acelerar()
+        {
+            return AlterarVelocidade(15);
+        }
+
+        //Ocultar o método da classe Pai com o 'new', ou seja, não precisa usar o virtual para fazer a sobreescrita
+        public new int Frear()
+        {
+            return AlterarVelocidade(-15);
+        }
     }
 
     internal class Heranca
@@ -79,7 +95,25 @@ namespace CursoCSharp.OO
             Console.WriteLine(carro2.Acelerar());
             Console.WriteLine(carro2.Acelerar());
             Console.WriteLine(carro2.Acelerar());
+            Console.WriteLine(carro2.Frear());
 
+            Console.WriteLine("Ferrari com o tipo carro...");
+            Carro carro3 = new Ferrari();
+            Console.WriteLine(carro3.Acelerar());
+            Console.WriteLine(carro3.Acelerar());
+            Console.WriteLine(carro3.Frear());
+
+            Console.WriteLine("Uno com o tipo carro...");
+            carro3 = new Uno(); // Polimorfismo. Uma mesma variável assumindo multiplas formas.
+            Console.WriteLine(carro3.Acelerar());
+            Console.WriteLine(carro3.Acelerar());
+            Console.WriteLine(carro3.Frear());
         }
     }
 }
+/*Diferença entre sobreescrita e ocultamento:
+ * O método sobreescrita de fato é chamado quando acesso a ferrari a partir da variável tipo carro.
+ * Entretando, o método frear que ocultou inicialmente quando tinha uma instância do tipo ferrari,
+ * ele funcionou. Mas quando instanciou ferrari do tipo carro, ele chamou o método de carro e não 
+ * de ferrari com isso, não funcionou da maneira que deveria funcionar.
+ */
