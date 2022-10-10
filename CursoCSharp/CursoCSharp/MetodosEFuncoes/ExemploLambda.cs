fb3f7b9b72a23b527bbeaafb3d4c3ae13c1ad47e
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-10-10 11:21:22 -0300

    Exemplo Lambda

---
+++ CursoCSharp/CursoCSharp/MetodosEFuncoes/ExemploLambda.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.MetodosEFuncoes
+{
+    internal class ExemploLambda
+    {//lambda é uma função anonima. E esta função, esta sendo armazenada na variável algoNoConsole
+     //Action é uma função que não tem retorno(void).
+        public static void Executar()
+        {
+            Action algoNoConsole = () => {
+                Console.WriteLine("Lambda com C#!");
+            };
+
+            algoNoConsole();
+
+            Func<int> jogarDado = () => {
+                Random random = new Random();
+                return random.Next(1, 7);
+            };
+
+            Console.WriteLine(jogarDado());
+
+            Func<int, string> conversorHex = (numero) => numero.ToString("X");
+            Console.WriteLine(conversorHex(1234));
+
+            //ele recebe 3 parâmetros e retorna uma string
+            Func<int, int, int, string> formatarData = (dia, mes, ano) =>
+                String.Format("{0:D2}/{1:D2}/{2:D4}", dia, mes, ano);
+            Console.WriteLine(formatarData(1,5,2022));
+
+        }
+    }
+}
