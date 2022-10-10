7acd9b0df8f429c9f71004cfe4ad9698e32bf6eb
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-10-10 10:21:32 -0300

    Sealed

---
+++ CursoCSharp/CursoCSharp/OO/Sealed.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.OO
+{//classe Sealed não permite herança.
+    sealed class SemFilho
+    {
+        public double ValorDaFortuna()
+        {
+            return 1_407_033.65;
+        }
+    }
+
+    //Não é possível herdar de uma classe selada.
+    //class SouFilho : semFilho
+    //{
+
+    //}
+
+    class Avo
+    {
+        public virtual bool HonrarNomeFamilia()
+        {
+            return true;
+        }
+    }
+
+    class Pai : Avo
+    {
+        public sealed override bool HonrarNomeFamilia()
+        {
+            return true;
+        }
+    }
+
+    class FilhoRebelde : Pai
+    {//não é possível sobreescrever um método sealed
+        
+        //public override bool HonrarNomeFamilia()
+        //{
+        //    return false;
+        //}
+    }
+
+    internal class Sealed
+    {
+        public static void Executar()
+        {
+            SemFilho semFilho = new SemFilho();
+            Console.WriteLine(semFilho.ValorDaFortuna());
+
+            FilhoRebelde filhoRebelde = new FilhoRebelde();
+            Console.WriteLine(filhoRebelde.HonrarNomeFamilia());
+        }
+    }
+}
