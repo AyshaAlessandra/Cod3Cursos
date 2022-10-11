a7d6bf75dfc64901923db193b36facf171bf0635
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-10-11 09:43:17 -0300

    Arquivo: Lendo Dados

--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -96,6 +96,7 @@ namespace CursoCSharp {
 
                 //Explorando API
                 {"Primeiro Arquivo - Api", PrimeiroArquivo.Executar},
+                {"Lendo Arquivos - Api", LendoArquivos.Executar},
 
             });
 
