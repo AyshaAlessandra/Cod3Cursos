a86c124609935c66af1a9165f79cf34a7cdd8ec8
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-10-11 12:59:27 -0300

    Usando DateTime

---
+++ CursoCSharp/CursoCSharp/Api/ExemploDateTime.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Api
+{
+    internal class ExemploDateTime
+    {
+        public static void Executar()
+        {
+            var dateTime = new DateTime(year: 2023, month: 2, day: 6);
+
+            Console.WriteLine(dateTime.Day);
+            Console.WriteLine(dateTime.Month);
+            Console.WriteLine(dateTime.Year);
+
+            //sem horas
+            var hoje = DateTime.Today;
+            Console.WriteLine(hoje);
+
+            // com hora
+            var diaAtual = DateTime.Now;
+            Console.WriteLine(diaAtual);
+            Console.WriteLine("Hora: " + diaAtual.Hour);
+            Console.WriteLine("Minute: " + diaAtual.Minute);
+            Console.WriteLine("Second: " + diaAtual.Second);
+
+            //mostrar outro dia 
+            var amanha = diaAtual.AddDays(1);
+            Console.WriteLine("\nAmanhã: " + amanha);
+
+            // dia anterior
+            var ontem = diaAtual.AddDays(-1);
+            Console.WriteLine("Ontem: " + ontem);
+
+            //Formando a data em formatos diferentes
+            Console.WriteLine("\n" + diaAtual.ToString("dd"));
+            Console.WriteLine( diaAtual.ToString("d"));
+            Console.WriteLine( diaAtual.ToString("D"));
+            //dia atual com a hora e segundos
+            Console.WriteLine(diaAtual.ToString("g"));
+            Console.WriteLine(diaAtual.ToString("G"));
+            Console.WriteLine("Modificando "+ diaAtual.ToString("dd-MM-yyyy HH:mm"));
+
+        }
+    }
+}
